709b91e3277ecb88a7d25a387f42eb9466b81a61
hungpm372 <hungpm372@gmail.com>
2024-05-15 21:49:21 +0700

    Create dashboard layout page

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Sidebar } from './_components/sidebar'
+import { OrgSidebar } from './_components/org-sidebar'
+import { Navbar } from './_components/navbar'
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className='h-full'>
+      <Sidebar />
+      <div className='pl-[60px] h-full'>
+        <div className='flex gap-x-3 h-full'>
+          <OrgSidebar />
+          <div className='h-full flex-1'>
+            <Navbar />
+            {children}
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default DashboardLayout
