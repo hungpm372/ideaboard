f25a61007a519c2139dd142679bc375dc3a0bfb5
hungpm372 <hungpm372@gmail.com>
2024-06-08 00:46:15 +0700

    feat: config i18n

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,30 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware } from '@clerk/nextjs'
+import createMiddleware from 'next-intl/middleware'
+
+const intlMiddleware = createMiddleware({
+  // A list of all locales that are supported
+  locales: ['en', 'de'],
+
+  // Used when no locale matches
+  defaultLocale: 'en'
+})
 
 // See https://clerk.com/docs/references/nextjs/auth-middleware
 // for more information about configuring your Middleware
 export default authMiddleware({
   // Allow signed out users to access the specified routes:
   publicRoutes: [],
-});
+  beforeAuth: (req) => {
+    return intlMiddleware(req)
+  }
+})
 
 export const config = {
   matcher: [
     // Exclude files with a "." followed by an extension, which are typically static files.
     // Exclude files in the _next directory, which are Next.js internals.
-    "/((?!.+\\.[\\w]+$|_next).*)",
+    '/((?!.+\\.[\\w]+$|_next).*)',
     // Re-include any files in the api or trpc folders that might have an extension
-    "/(api|trpc)(.*)",
-  ],
-};
+    '/(api|trpc)(.*)'
+  ]
+}
